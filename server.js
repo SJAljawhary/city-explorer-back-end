bd1b072937f6f46a258922a0cb3d24dc1b7c8215
SJAljawhary <sndjehad@gmail.com>
2021-07-06 22:07:13 +0300

    editing

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ function weatherFunction(req, res) {
     let searchQuery = req.query.cityName;
 
 
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&${searchQuery}`
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`
 
     //https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&${searchQuery}
 
